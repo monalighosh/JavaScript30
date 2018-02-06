b4b971c31124a8ed2d51ed16b7ddcfcc3956eaeb
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-07 00:23:39 +1100

    Formatting click effect styles and script file

--- Day01/js/main.js
+++ Day01/js/main.js
@@ -1,18 +1,20 @@
 "use strict";
-const audioList = document.querySelectorAll("audio");
 
-// Adding click event listener to each key
-document.addEventListener("keypress", playSound);
+// Adding keydown event listener to each key
+window.addEventListener("keydown", playSound);
 
 // Function to play sound on key press
 function playSound(e){
-  console.log(e.key);
-  // this.classList.add("play");
-  const keyCode = e.charCode;
-   audioList.forEach(function(audio){
-   const audioKey = parseInt(audio.dataset.key);
-    keyCode === audioKey ? audio.play() : false;
-  });
-
+  const audioEle = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keyEle = document.querySelector(`li[data-key="${e.keyCode}"]`);
+  if (audioEle === null) { // For keys without sound effect
+    return;
+  } 
+  audioEle.currentTime = 0; // Rewinds audio 
+  audioEle.play(); // Plays the audio
+  keyEle.classList.add("play"); // Adds new class to the key for click effect
 }
+
+// Function to remove click effect class from the keys
+const drumKeys = document.querySelectorAll("[class='drum-keys__items']");
 
