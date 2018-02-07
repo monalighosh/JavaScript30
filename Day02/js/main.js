5b7dcbe7a45c6fd69cc8a915608f9b9bb1de9b71
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-07 23:22:31 +1100

    Formatting script

--- Day02/js/main.js
+++ Day02/js/main.js
@@ -3,11 +3,18 @@ const hourHand = document.querySelector(".hour");
 const minutesHand = document.querySelector(".minute");
 const secondsHand = document.querySelector(".seconds");
 
-function clockHandsMovement() {
-  const today = new Date();
-  const seconds = today.getSeconds();
-  const minutes = today.getMinutes();
-  const hours = today.getHours();
+// Clock function
+function clockTicking() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  /* 
+  Calculates the clock hand's rotation degree 
+  formula: ((currentTime / maxTime) * 360) + 90;
+  where, 360 is a total degree roation and 90 is intial offset 
+  */
   const secondDeg = ((seconds / 60) * 360) + 90;
   const minuteDeg = ((minutes / 60) * 360) + 90;
   const hourDeg = ((hours / 12) * 360) + 90;
@@ -15,4 +22,4 @@ function clockHandsMovement() {
   minutesHand.style.transform = `translateX(23%) rotate(${minuteDeg}deg)`;
   hourHand.style.transform = `translateX(40%) rotate(${hourDeg}deg)`;
 }
-setInterval(clockHandsMovement, 1000);
+setInterval(clockTicking, 1000);
