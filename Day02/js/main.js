91ecf72e5c94c18c2d5bb592344446045dfd08df
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-08 00:44:33 +1100

    Formatting styles

--- Day02/js/main.js
+++ Day02/js/main.js
@@ -9,7 +9,6 @@ function clockTicking() {
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
-
   /* 
   Calculates the clock hand's rotation degree 
   formula: ((currentTime / maxTime) * 360) + 90;
@@ -18,8 +17,11 @@ function clockTicking() {
   const secondDeg = ((seconds / 60) * 360) + 90;
   const minuteDeg = ((minutes / 60) * 360) + 90;
   const hourDeg = ((hours / 12) * 360) + 90;
+
   secondsHand.style.transform = `translateX(9%) rotate(${secondDeg}deg)`;
+  seconds === 0 ? secondsHand.style.transition = `all 0s` : secondsHand.style.transition = `all 0.05s cubic-bezier(0.46, 2.7, 0.68, 1.6)`;
   minutesHand.style.transform = `translateX(23%) rotate(${minuteDeg}deg)`;
+  minutes === 0 ? minutesHand.style.transition = `all 0s` : minutesHand.style.transition = `all 0.05s cubic-bezier(0.46, 2.7, 0.68, 1.6)`;
   hourHand.style.transform = `translateX(40%) rotate(${hourDeg}deg)`;
 }
 setInterval(clockTicking, 1000);
